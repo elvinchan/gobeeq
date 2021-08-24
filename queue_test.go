7a828fd3417dbc06121f94c87726c68a8dd8699b
elvinchan <elvinchan@foxmail.com>
2021-08-24 16:27:39 +0800

    Add unit tests

--- queue_test.go
+++ queue_test.go
@@ -474,3 +474,209 @@ func TestCheckHealth(t *testing.T) {
 	assert.Equal(t, int64(5), status.Keys[keyDelayed])
 	assert.Equal(t, int64(10), status.NewestJobId)
 }
+
+func TestQueuePrefix(t *testing.T) {
+	t.Parallel()
+
+	name := "test-queue-prefix"
+	prefix := "beequeue"
+	ctx := context.Background()
+	queue, err := NewQueue(ctx, name, client, WithPrefix(prefix))
+	assert.NoError(t, err)
+
+	_, err = queue.CreateJob(mockData(0)).Save(ctx)
+	assert.NoError(t, err)
+
+	keys, err := client.Keys(ctx, prefix+"*").Result()
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []string{
+		"beequeue:test-queue-prefix:id",
+		"beequeue:test-queue-prefix:jobs",
+		"beequeue:test-queue-prefix:waiting",
+	}, keys)
+}
+
+func TestQueueRemoveOnSuccess(t *testing.T) {
+	t.Run("No", func(t *testing.T) {
+		t.Parallel()
+		name := "test-queue-remove-on-success-no"
+		ctx := context.Background()
+		queue, err := NewQueue(ctx, name, client)
+		assert.NoError(t, err)
+
+		err = queue.Process(func(ctx Context) error {
+			return nil
+		})
+		assert.NoError(t, err)
+
+		j, err := queue.CreateJob(mockData(0)).Save(ctx)
+		assert.NoError(t, err)
+
+		waitSync()
+		j, err = queue.GetJob(ctx, j.Id)
+		assert.NoError(t, err)
+		assert.Equal(t, mockData(0), j.data)
+	})
+
+	t.Run("Yes", func(t *testing.T) {
+		t.Parallel()
+		name := "test-queue-remove-on-success-yes"
+		ctx := context.Background()
+		queue, err := NewQueue(ctx, name, client, WithRemoveOnSuccess(true))
+		assert.NoError(t, err)
+
+		err = queue.Process(func(ctx Context) error {
+			return nil
+		})
+		assert.NoError(t, err)
+
+		j, err := queue.CreateJob(mockData(0)).Save(ctx)
+		assert.NoError(t, err)
+
+		waitSync()
+		_, err = queue.GetJob(ctx, j.Id)
+		assert.EqualError(t, err, redis.Nil.Error())
+	})
+}
+
+func TestQueueRemoveOnFailure(t *testing.T) {
+	t.Run("No", func(t *testing.T) {
+		t.Parallel()
+		name := "test-queue-remove-on-failure-no"
+		ctx := context.Background()
+		queue, err := NewQueue(ctx, name, client)
+		assert.NoError(t, err)
+
+		err = queue.Process(func(ctx Context) error {
+			t.Log("processing job:", ctx.GetId())
+			return errors.New("fail as expected")
+		})
+		assert.NoError(t, err)
+
+		j, err := queue.CreateJob(mockData(0)).Save(ctx)
+		assert.NoError(t, err)
+
+		waitSync()
+		j, err = queue.GetJob(ctx, j.Id)
+		assert.NoError(t, err)
+		assert.Equal(t, mockData(0), j.data)
+	})
+
+	t.Run("Yes", func(t *testing.T) {
+		t.Parallel()
+		name := "test-queue-remove-on-failure-yes"
+		ctx := context.Background()
+		queue, err := NewQueue(ctx, name, client, WithRemoveOnFailure(true))
+		assert.NoError(t, err)
+
+		err = queue.Process(func(ctx Context) error {
+			t.Log("processing job:", ctx.GetId())
+			return errors.New("fail as expected")
+		})
+		assert.NoError(t, err)
+
+		j, err := queue.CreateJob(mockData(0)).Save(ctx)
+		assert.NoError(t, err)
+
+		waitSync()
+		_, err = queue.GetJob(ctx, j.Id)
+		assert.EqualError(t, err, redis.Nil.Error())
+	})
+
+	t.Run("YesWithRetry", func(t *testing.T) {
+		t.Parallel()
+		name := "test-queue-remove-on-failure-yes-with-retry"
+		ctx := context.Background()
+		queue, err := NewQueue(ctx, name, client,
+			WithActivateDelayedJobs(true, nil), WithRemoveOnFailure(true))
+		assert.NoError(t, err)
+
+		retries := 3
+		ch := make(chan struct{})
+		err = queue.Process(func(ctx Context) error {
+			t.Log("processing job:", ctx.GetId())
+			ch <- struct{}{}
+			return errors.New("fail as expected")
+		})
+		assert.NoError(t, err)
+
+		j, err := queue.CreateJob(mockData(0)).Retries(retries).Save(ctx)
+		assert.NoError(t, err)
+
+		for i := 0; i <= retries; i++ {
+			<-ch
+			waitSync() // wait for job finish and data sync to Redis
+			j, err = queue.GetJob(ctx, j.Id)
+			if i < retries {
+				assert.NoError(t, err)
+				assert.Equal(t, mockData(0), j.data)
+			} else {
+				assert.EqualError(t, err, redis.Nil.Error())
+				assert.Nil(t, j)
+			}
+		}
+	})
+}
+
+func TestQueueOnJobRetrying(t *testing.T) {
+	t.Parallel()
+	name := "test-queue-on-job-retrying"
+	ctx := context.Background()
+
+	ch := make(chan struct{})
+	e := "fail as expected"
+	id := "1"
+	queue, err := NewQueue(ctx, name, client,
+		WithSendEvents(true),
+		WithOnJobRetrying(func(jobId string, err error) {
+			assert.Equal(t, id, jobId)
+			assert.EqualError(t, err, e)
+			ch <- struct{}{}
+		}),
+	)
+	assert.NoError(t, err)
+
+	retries := 3
+	_, err = queue.CreateJob(nil).SetId(id).Retries(retries).Save(ctx)
+	assert.NoError(t, err)
+
+	err = queue.Process(func(ctx Context) error {
+		t.Log("processing job:", ctx.GetId())
+		return errors.New(e)
+	})
+	assert.NoError(t, err)
+
+	for i := 0; i < retries; i++ {
+		<-ch
+	}
+}
+
+func TestQueueOnJobFailed(t *testing.T) {
+	t.Parallel()
+	name := "test-queue-on-job-failed"
+	ctx := context.Background()
+
+	ch := make(chan struct{})
+	e := "fail as expected"
+	id := "1"
+	queue, err := NewQueue(ctx, name, client,
+		WithSendEvents(true),
+		WithOnJobFailed(func(jobId string, err error) {
+			assert.Equal(t, id, jobId)
+			assert.EqualError(t, err, e)
+			ch <- struct{}{}
+		}),
+	)
+	assert.NoError(t, err)
+
+	_, err = queue.CreateJob(nil).SetId(id).Save(ctx)
+	assert.NoError(t, err)
+
+	err = queue.Process(func(ctx Context) error {
+		t.Log("processing job:", ctx.GetId())
+		return errors.New(e)
+	})
+	assert.NoError(t, err)
+
+	<-ch
+}
